d1e9f0a04f6494813644762818ee36fcb7c10110
Sumit <sumitmahajan2001@gmail.com>
2020-10-10 12:35:34 +0530

    bcrypt, jwt

--- src/infrastructure/security/access_token_manager.ts
+++ src/infrastructure/security/access_token_manager.ts
@@ -1,6 +1,14 @@
 import { IAccessTokenManager } from "../../application/abstracts/access_token_manager_interface";
+import  jwt from "jsonwebtoken";
 
 export class AccessTokenManager implements IAccessTokenManager {
-  generate: (userId: string) => Promise<string>;
+  generate = (userId: string): Promise<string> => {
+    var token : String;
+    token = jwt.sign(userId, 'secretkey');
+    return new Promise((reject, resolve) =>{
+      resolve(token);
+    })
+  };
+
   verify: (token: string) => Promise<boolean>;
 }
--- src/infrastructure/security/encrypter.ts
+++ src/infrastructure/security/encrypter.ts
@@ -1,6 +1,28 @@
 import { IEncrypter } from "../../application/abstracts/encrypter_interface";
+import bcrypt from "bcrypt";
 
 export class Encrypter implements IEncrypter {
-  encrypt: (value: string) => Promise<string>;
-  compare: (value1: string, value2: string) => Promise<boolean>;
+  encrypt = (value: string): Promise<string> => {
+    return new Promise((reject, resolve) => {
+      bcrypt.hash(value, 10, (err : Error, hashed : string) => {
+        if (err) {
+          throw err;
+        } else {
+          resolve(hashed);
+        }
+      })
+    });
+  };
+
+  compare = (value1: string, value2: string): Promise<boolean> => {
+    return new Promise((reject, resolve) => {
+      bcrypt.compare(value1, value2, (err : Error, res : boolean) => {
+        if (err) {
+          throw err;
+        } else {
+          resolve(res);
+        }
+      })
+    });
+  };
 }
