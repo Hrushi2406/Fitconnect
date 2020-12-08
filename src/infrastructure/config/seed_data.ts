2e473b51ee6aa11e1f942549bbaf10bd7cb63a0a
Hrushikesh <kukularehrushi@gmail.com>
2020-12-08 12:09:49 +0530

    Changes default images

--- src/infrastructure/config/seed_data.ts
+++ src/infrastructure/config/seed_data.ts
@@ -1,4 +1,4 @@
-const puppeteer = require('puppeteer');
+const puppeteer = require("puppeteer");
 import faker from "faker";
 import { Trainer, ITrainer } from "../../domain/entities/trainer";
 import { IDGenerator } from "../core/id_generator";
@@ -6,7 +6,72 @@ import driver from "./db_config";
 import { Plan } from "../../domain/entities/plans";
 
 export async function seedTrainer(): Promise<void> {
-  const Imgs = await scrapeImages("https://www.gettyimages.in/photos/fitness-instructor?mediatype=photography&phrase=fitness%20instructor&sort=mostpopular");
+  // const Imgs = await scrapeImages(
+  //   "https://www.gettyimages.in/photos/fitness-instructor?mediatype=photography&phrase=fitness%20instructor&sort=mostpopular"
+  // );
+
+  const Imgs = [
+    "https://media.gettyimages.com/photos/stretching-in-the-gym-with-a-personal-trainer-picture-id1040504314?k=6&m=1040504314&s=612x612&w=0&h=xQe_eFh1cgxzPLg0yA3WUneK00iza21EfpFF4R9OfyY=",
+    "https://media.gettyimages.com/photos/young-woman-doing-stretching-exercise-picture-id925395994?k=6&m=925395994&s=612x612&w=0&h=u8hrs7y1bVzwm4rp6-UBC3YwVSrSbOx8OvO-lDkqCUw=",
+    "https://media.gettyimages.com/photos/come-on-one-more-pushup-picture-id1138386337?k=6&m=1138386337&s=612x612&w=0&h=LeWhi4gKpMBYmFflbrweAwKLoy2XLC6yJEytfFVq1Ys=",
+    "https://media.gettyimages.com/photos/adult-woman-exercising-at-the-gym-with-a-personal-trainer-picture-id852401728?k=6&m=852401728&s=612x612&w=0&h=I898RH_z7xgce163qmhmHVnh3GCANN7Yp4yjCb1nXI4=",
+    "https://media.gettyimages.com/photos/happy-senior-woman-having-fun-with-her-coach-on-exercising-class-in-a-picture-id909428568?k=6&m=909428568&s=612x612&w=0&h=AAIcHOJwOfZHG-Z3TRmt1WLSqzp6GCsQMhGHFnOjxGA=",
+    "https://media.gettyimages.com/photos/fitness-trainer-at-gym-picture-id1072395722?k=6&m=1072395722&s=612x612&w=0&h=SEymEzcNs1YqbLDwEDzZtFLXLyTzBxfiOBVoniNoKuM=",
+    "https://media.gettyimages.com/photos/female-fitness-instructor-going-thru-written-training-program-with-picture-id1067723968?k=6&m=1067723968&s=612x612&w=0&h=sstzeeTZBtPQZ1qN1ZarFisDblt001SlQBcvehgvN7k=",
+    "https://media.gettyimages.com/photos/fitness-instructor-guiding-young-woman-when-she-exercises-picture-id1137413164?k=6&m=1137413164&s=612x612&w=0&h=_1ReBrtgs3EzsYSxCI7P8B4kpuMN3QGguUdLIoVFEUc=",
+    "https://media.gettyimages.com/photos/happy-personal-trainer-working-at-the-gym-picture-id852401732?k=6&m=852401732&s=612x612&w=0&h=WTwWXkG-GMshYAHmdEeROWNDa6ABmHR3uZvNadC4A0g=",
+    "https://media.gettyimages.com/photos/he-pushes-her-to-do-her-best-picture-id1051628014?k=6&m=1051628014&s=612x612&w=0&h=uQMuSAUTt8bqgP5MTtzSxFFn03WqABPUk0i7istBEu4=",
+    "https://media.gettyimages.com/photos/sporty-woman-doing-pushups-under-supervision-of-personal-trainer-picture-id881592358?k=6&m=881592358&s=612x612&w=0&h=THpevroTg4ojvMdJq5xnygUWq1YntuCYyik-nN313gM=",
+    "https://media.gettyimages.com/photos/portrait-of-a-beautiful-woman-at-the-gym-picture-id856797530?k=6&m=856797530&s=612x612&w=0&h=_hDfYO_exuYj3jq157FAr_Al6dA-lCi7lLDtwKmFh48=",
+    "https://media.gettyimages.com/photos/couple-of-fitness-models-doing-the-pushups-picture-id869062004?k=6&m=869062004&s=612x612&w=0&h=b1VX5SsFhkVU7BRT8VGsAD8Iz1dyyI-Ghd5vftI1184=",
+    "https://media.gettyimages.com/photos/athletic-woman-doing-battlerope-exercise-with-personal-trainer-at-gym-picture-id982408624?k=6&m=982408624&s=612x612&w=0&h=vzhdt4irrJ0ZsHDNv1hYCHCI2aUakI81vttlljmH5cc=",
+    "https://media.gettyimages.com/photos/personal-trainer-caring-woman-with-her-workout-picture-id531326798?k=6&m=531326798&s=612x612&w=0&h=z0mUrcw7ciqjiSVrbJmGAR3mSXKqEQGhzO1UlDkS5oc=",
+    "https://media.gettyimages.com/photos/young-fitness-instructor-reading-a-training-plan-in-a-gym-picture-id942145636?k=6&m=942145636&s=612x612&w=0&h=Ei5D_WMX6lSGLTxmVTTtParfD0oEZSl8UrBMZWLV3E4=",
+    "https://media.gettyimages.com/photos/portrait-of-chinese-personal-trainer-in-gym-picture-id1018043738?k=6&m=1018043738&s=612x612&w=0&h=8-9NE2oJ7PB2hDcFg_GJldbVfXcQSJZik20fAarHp8A=",
+    "https://media.gettyimages.com/photos/personal-weight-training-in-the-gym-picture-id1084251084?k=6&m=1084251084&s=612x612&w=0&h=2DCX5vi4fUtp-ED9aAcLQI0GL12K46syoiKS6fNUtII=",
+    "https://media.gettyimages.com/photos/male-athlete-sitting-on-exercise-mat-and-making-vlog-at-gym-picture-id1176295972?k=6&m=1176295972&s=612x612&w=0&h=ALhiG5S0__I8rnkIt4LXnU7tpdloPXKqMvlUwa_KcTA=",
+    "https://media.gettyimages.com/photos/personal-trainer-guiding-woman-doing-barbell-squats-at-gym-picture-id616121640?k=6&m=616121640&s=612x612&w=0&h=Pn6P7WzNsh3fHJz5KAT7LULePxPtxNP3s5Y2FtrnmrA=",
+    "https://media.gettyimages.com/photos/trainer-and-female-client-talking-in-gym-picture-id1144036339?k=6&m=1144036339&s=612x612&w=0&h=lH8xWPahsQIN9fNvLxLfONQFSufpPttel1pZjGDKLGs=",
+    "https://media.gettyimages.com/photos/personal-trainer-helping-mature-woman-at-gym-picture-id1047643096?k=6&m=1047643096&s=612x612&w=0&h=DZlb1nz23B7NnIFlX2t7QYTCEACNfRawCm_F2wPmd64=",
+    "https://media.gettyimages.com/photos/gym-workout-picture-id619274128?k=6&m=619274128&s=612x612&w=0&h=IYy5J172Mk3atM9lfVGPQrQkOsTJG_6opOxVWHg5IJ0=",
+    "https://media.gettyimages.com/photos/woman-exercising-with-dumbbells-in-squat-position-picture-id897564884?k=6&m=897564884&s=612x612&w=0&h=y8AVfmb7HOyc_uSNpwIrUSnmbGgRGYSqdaJe4QUOr2s=",
+    "https://media.gettyimages.com/photos/young-woman-doing-stretching-exercise-picture-id918949496?k=6&m=918949496&s=612x612&w=0&h=vosaEshGZAtxlEFKiiyxWifgTNibZm9Sdmbx7WMLpus=",
+    "https://media.gettyimages.com/photos/personal-trainer-timing-his-client-in-a-plank-position-picture-id1015125722?k=6&m=1015125722&s=612x612&w=0&h=uMhmQL_uks_77YcWAWSM7h6W7NLole-hHQD0nSa2mgM=",
+    "https://media.gettyimages.com/photos/fitness-people-working-out-with-battle-ropes-picture-id616122214?k=6&m=616122214&s=612x612&w=0&h=XvF8tzmQGsngmaNnvzbMw1D2qp2bc1g6lNKXGRP1KXM=",
+    "https://media.gettyimages.com/photos/portrait-of-fitness-trainer-at-gym-picture-id948515214?k=6&m=948515214&s=612x612&w=0&h=GA8CEwUMOTNyK7sWghsv3o2Yu8tYOsy-igdoem_zKZc=",
+    "https://media.gettyimages.com/photos/fitness-trainer-teaching-girl-weightlifting-picture-id1049932176?k=6&m=1049932176&s=612x612&w=0&h=tT6I7xcOLrtpS2UhG6YZwRNtpq5dEBKSU2Ke8K5Q8Tk=",
+    "https://media.gettyimages.com/photos/personal-trainer-helping-senior-woman-at-gym-picture-id1047643090?k=6&m=1047643090&s=612x612&w=0&h=ipAaI6e66IUdWh8aazJ_aHlHfdTJzZMHCVcSTDBOhUE=",
+    "https://media.gettyimages.com/photos/fitness-instructor-helping-late-teen-girl-during-exercise-picture-id943532644?k=6&m=943532644&s=612x612&w=0&h=gdarPO5W_v_oqPiUeWOwNdVK0HZ_f9jbhsZUCLsouks=",
+    "https://media.gettyimages.com/photos/young-athletic-woman-having-weight-training-with-fitness-instructor-picture-id618194022?k=6&m=618194022&s=612x612&w=0&h=kn2Qt21lB7zJGIBsE01o1aV5l0ee0itvddb3Z8X83WU=",
+    "https://media.gettyimages.com/photos/people-exercising-in-a-gym-with-medicine-balls-picture-id1145611494?k=6&m=1145611494&s=612x612&w=0&h=bdxgf9OUgYlMH6IAuV-dtZTseoHSWbaVyTt-kLu_3YQ=",
+    "https://media.gettyimages.com/photos/fitness-and-technology-picture-id902286308?k=6&m=902286308&s=612x612&w=0&h=OaZGMUabHujqP-bPl37PGSr433XplOQpBNRCNjuxbns=",
+    "https://media.gettyimages.com/photos/young-woman-and-fitness-instructor-on-cross-training-in-gym-picture-id638039998?k=6&m=638039998&s=612x612&w=0&h=GvQeFr9MEubbid33B4M35tyaJtHkr3JFAcoNVSQqH2U=",
+    "https://media.gettyimages.com/photos/testing-their-endurence-picture-id654427364?k=6&m=654427364&s=612x612&w=0&h=l38DRu9H0fvh4Xbu-MwY9rwl2HP1sYVwYJ4kaBunoVg=",
+    "https://media.gettyimages.com/photos/see-what-happens-when-you-dont-give-up-picture-id1175815366?k=6&m=1175815366&s=612x612&w=0&h=_WvcAzzJA_QUqk6x-zQiH7EuItyEvwmqtcMfUiOkEA0=",
+    "https://media.gettyimages.com/photos/fitness-instructor-taking-a-class-picture-id904637630?k=6&m=904637630&s=612x612&w=0&h=vttfxmxMRWeA19UPrBb1wAIl_G46_aJnNZ3Bot80H5g=",
+    "https://media.gettyimages.com/photos/helping-him-achieve-great-progress-picture-id682517288?k=6&m=682517288&s=612x612&w=0&h=4_ZRIW94HdH2RInayiD0t4oYullggBWGp-DDzjx1uvk=",
+    "https://media.gettyimages.com/photos/workout-with-personal-trainer-on-rowing-machine-at-gym-picture-id897564406?k=6&m=897564406&s=612x612&w=0&h=Evws4iHRkPJLbI1cVKZZmUNDSDtxJqyEcb3AAyb7K7c=",
+    "https://media.gettyimages.com/photos/keep-this-parallel-to-the-ground-picture-id844459504?k=6&m=844459504&s=612x612&w=0&h=Dfz3V1WCiLg9Y6p1xDbf1fqVe1tV8YDphzR2t4zx1oY=",
+    "https://media.gettyimages.com/photos/woman-doing-arm-exercises-with-suspension-straps-at-gym-picture-id621372500?k=6&m=621372500&s=612x612&w=0&h=BXZtbFJ3zdnoUcCkujW8vx9njJgQhxrHfXetDrOS62w=",
+    "https://media.gettyimages.com/photos/trainer-watching-woman-running-ladder-picture-id738777325?k=6&m=738777325&s=612x612&w=0&h=nqn8GT8IfhLJkxzLv0lI3iO5jMNc_PCKAFRJlktTbPg=",
+    "https://media.gettyimages.com/photos/woman-lifting-barbell-while-personal-trainer-assisting-her-in-gy-picture-id576573040?k=6&m=576573040&s=612x612&w=0&h=S-J2h6aRu0ecoqMx4lkN3Bn_fm638I-3kNFLLlfn9Q4=",
+    "https://media.gettyimages.com/photos/portrait-of-a-female-personal-trainer-in-the-gym-picture-id1040495528?k=6&m=1040495528&s=612x612&w=0&h=bpC1dFuS53OgQa5whIC0QcvTQPIJTpjjYEP_YVam28Y=",
+    "https://media.gettyimages.com/photos/africanamerican-fitness-instructor-helping-senior-woman-picture-id1140884096?k=6&m=1140884096&s=612x612&w=0&h=uRrmxob0QhpjEu2KhdfB83FVI-sbJEeCnxKti0GlHvE=",
+    "https://media.gettyimages.com/photos/woman-and-personal-trainer-making-exercise-plan-in-gym-picture-id1078991800?k=6&m=1078991800&s=612x612&w=0&h=34U7dkJXrkN6T7fBrcOwC5SmOrZcheRGcWnxWqUxb2M=",
+    "https://media.gettyimages.com/photos/happy-fitness-instructor-with-training-plan-in-a-health-club-picture-id925188400?k=6&m=925188400&s=612x612&w=0&h=6QF2zJmF2bVr27EcgbzKQIRRDsb4X4pSLEEqGiAdipg=",
+    "https://media.gettyimages.com/photos/personal-trainer-helping-woman-work-out-picture-id96240487?k=6&m=96240487&s=612x612&w=0&h=q_6Ajd-JOnrRJJlFLZVCVB6IikrUWQNKlD_03AUrXxg=",
+    "https://media.gettyimages.com/photos/personal-trainer-coaching-a-female-athlete-while-lifting-weights-picture-id816802320?k=6&m=816802320&s=612x612&w=0&h=F8hj9QxwlW899mddAlmZKQXSqS8bFIO9K0j-TE0hr1E=",
+    "https://media.gettyimages.com/photos/little-help-from-her-trainer-picture-id530761466?k=6&m=530761466&s=612x612&w=0&h=zk2EPhjwR6aWf_f-gwKZnzTYzCjwFoIEPv4-C-x1Cbo=",
+    "https://media.gettyimages.com/photos/workout-in-gym-after-pandemic-picture-id1227597612?k=6&m=1227597612&s=612x612&w=0&h=YxDyYJH7PSEycqOFPC2ewon79vR_csvGHXuXHu5laHs=",
+    "https://media.gettyimages.com/photos/portrait-of-a-personal-trainer-in-the-gym-picture-id1040501222?k=6&m=1040501222&s=612x612&w=0&h=Jicbgmi2gWC1cYMtm4j0V0wV196LbSwFHtvcpPo_Jp4=",
+    "https://media.gettyimages.com/photos/mature-woman-running-on-treadmill-while-being-coached-by-personal-picture-id1159498003?k=6&m=1159498003&s=612x612&w=0&h=bTf99uYOL1kQCJF3YiIo56KEts8n1kxEVBLxfXX7b54=",
+    "https://media.gettyimages.com/photos/helping-him-to-achieve-his-fitness-goals-picture-id1051627106?k=6&m=1051627106&s=612x612&w=0&h=Ughc_kgwQoKyPP1tDT_w-YHWF2Q1k2whvN2e9K0pYeI=",
+    "https://media.gettyimages.com/photos/personal-trainer-with-man-on-rowing-machine-in-gymnasium-picture-id147205518?k=6&m=147205518&s=612x612&w=0&h=vfYqD-Epi2V-Qat7-XcVsAnk83W34RMBsJywi6WuvS4=",
+    "https://media.gettyimages.com/photos/woman-with-her-trainer-working-out-in-gym-picture-id735893671?k=6&m=735893671&s=612x612&w=0&h=Z3PAr0EVnEDsjc-xztO_XgkgYEMVYtjO4Qa8qJSlsbU=",
+    "https://media.gettyimages.com/photos/smiling-fitness-trainer-talking-to-young-woman-in-a-gym-picture-id544576336?k=6&m=544576336&s=612x612&w=0&h=xYWQ_v9bEb0oB5XAafiKCKXWzcsTo1pA69a-o6O8Ows=",
+    "https://media.gettyimages.com/photos/senior-woman-exercising-at-the-gym-with-a-personal-trainer-picture-id1010884934?k=6&m=1010884934&s=612x612&w=0&h=Lh7F2agiBDD5EcKhoGci8elgvTqG5ob8tG9blR8MaQs=",
+    "https://media.gettyimages.com/photos/woman-jumping-on-box-with-fitness-trainer-picture-id616121156?k=6&m=616121156&s=612x612&w=0&h=JtoFyWC-MQiYzMREZoe9Y7pgz0BKG8dqmBOR61zSrq8=",
+  ];
 
   const session = driver.session();
 
@@ -163,32 +228,31 @@ function createTrainer(Imgs: any, index: number): Trainer {
   return trainer;
 }
 
-async function scrapeImages(url : string): Promise<any> {
+async function scrapeImages(url: string): Promise<any> {
   let browser = await puppeteer.launch({
-      headless: true,
-      args: ["--disable-setuid-sandbox"],
-      'ignoreHTTPSErrors': true
+    headless: true,
+    args: ["--disable-setuid-sandbox"],
+    ignoreHTTPSErrors: true,
   });
 
   const page = await browser.newPage();
   page.goto(url);
   await page.setViewport({
-      width: 1000,
-      height: 800
+    width: 1000,
+    height: 800,
   });
 
-  await page.waitForSelector('.search-content__gallery');
+  await page.waitForSelector(".search-content__gallery");
 
   try {
-      const imgUrl = await page.$$eval(
-          '.search-content__gallery .gallery-mosaic-asset__figure img', 
-          (imgs : any) => imgs.map((img: any) => img.src)
-      );
-      console.log('Scrapped Trainer Images');
-      
-      return imgUrl;
-  } catch (err) {
-      console.log("Went wrong", err);
-  }
+    const imgUrl = await page.$$eval(
+      ".search-content__gallery .gallery-mosaic-asset__figure img",
+      (imgs: any) => imgs.map((img: any) => img.src)
+    );
+    console.log("Scrapped Trainer Images");
 
+    return imgUrl;
+  } catch (err) {
+    console.log("Went wrong", err);
+  }
 }
--- src/infrastructure/config/user_data.ts
+++ src/infrastructure/config/user_data.ts
@@ -1,4 +1,4 @@
-const puppeteer = require('puppeteer');
+const puppeteer = require("puppeteer");
 import faker from "faker";
 import User from "../../domain/entities/user";
 import { Encrypter } from "../../infrastructure/security/encrypter";
@@ -7,7 +7,69 @@ import driver from "./db_config";
 import { UserRepository } from "../../data_provider/repository/user_repository";
 
 export async function seedUser(): Promise<void> {
-  const Imgs = await scrapeImages("https://www.gettyimages.in/photos/happy-person?family=creative&license=rf&phrase=happy%20person&sort=mostpopular#license");
+  // const Imgs = await scrapeImages("https://www.gettyimages.in/photos/happy-person?family=creative&license=rf&phrase=happy%20person&sort=mostpopular#license");
+  const Imgs = [
+    "https://media.gettyimages.com/photos/joyous-woman-infront-of-wall-picture-id981594838?k=6&m=981594838&s=612x612&w=0&h=L3Aq4sxRdLAqbQVmeT52d1nnqGyNu_qPHUjEPcUmoo8=",
+    "https://media.gettyimages.com/photos/using-mobile-phone-picture-id1147785920?k=6&m=1147785920&s=612x612&w=0&h=ZhXW2w8LDfxRbRKK-3BXWIVl1UwHmiLrV-LPHww-ZF8=",
+    "https://media.gettyimages.com/photos/enjoying-the-fresh-sea-air-picture-id468906855?k=6&m=468906855&s=612x612&w=0&h=dXLefVTpDhCoO7LMiNYKnl-k30swAywVO-iMqoc5wsk=",
+    "https://media.gettyimages.com/photos/mature-man-playing-with-his-little-daughter-in-nature-picture-id1023147086?k=6&m=1023147086&s=612x612&w=0&h=2ut2H06i8kmAOemdB_BjQmz8ZLgIWK5QtTHxVS3Q2Mg=",
+    "https://media.gettyimages.com/photos/portrait-of-happy-young-woman-enjoying-sunlight-picture-id1134454088?k=6&m=1134454088&s=612x612&w=0&h=pT4ynfqRPOn_5j46lqGc08UZJuFH4JtKm3X4WhBevuM=",
+    "https://media.gettyimages.com/photos/the-suns-up-and-so-is-my-mood-picture-id911999972?k=6&m=911999972&s=612x612&w=0&h=dimamyIp4K_FgUZGEdHI3ZmGptgJPtm0vzfbxIQ7vM4=",
+    "https://media.gettyimages.com/photos/nothing-inspires-happiness-like-love-picture-id1094338222?k=6&m=1094338222&s=612x612&w=0&h=Aa85rto1n0SamxoW1g2GQ9ctcdrKpGNNlbAwpt3muZg=",
+    "https://media.gettyimages.com/photos/young-woman-with-dog-picture-id1060529042?k=6&m=1060529042&s=612x612&w=0&h=ofC5lNecobvOgQS_QtbWgY3gub_58t2EbcCJyRB9o9s=",
+    "https://media.gettyimages.com/photos/laughing-young-woman-lying-on-a-bench-using-cell-phone-picture-id1139713930?k=6&m=1139713930&s=612x612&w=0&h=ofIQ2icxKQ0OEnlJY7-ajXvVQMvLnuE9uFXQf6MoBNg=",
+    "https://media.gettyimages.com/photos/colourful-studio-portrait-of-a-young-man-picture-id1146910475?k=6&m=1146910475&s=612x612&w=0&h=8oe7NoRxvLBSy3uzc4XVdg8jFmJgMJoLSrbVh-8EJgk=",
+    "https://media.gettyimages.com/photos/cracking-codes-is-my-speciality-picture-id993565470?k=6&m=993565470&s=612x612&w=0&h=SaitH0UWEIe7nTQImPXkoQVA4cgxVY2_sIAO42h-sfc=",
+    "https://media.gettyimages.com/photos/working-hard-no-matter-the-time-picture-id912944158?k=6&m=912944158&s=612x612&w=0&h=B04Y7_2p5cbT06iBqr_Ka_ll1Jj_yKMxxF7IPHV_eh4=",
+    "https://media.gettyimages.com/photos/smiling-businesswoman-at-work-picture-id1092806334?k=6&m=1092806334&s=612x612&w=0&h=QFciT2-ETPhrtXIdaSaBhMCK80qXkZ0gtBTvad_tQYw=",
+    "https://media.gettyimages.com/photos/father-and-daughter-laughing-in-bedroom-picture-id1144518606?k=6&m=1144518606&s=612x612&w=0&h=D5R065nw0QzkSUCthGqC_CenuiBXUCfT9uQmIoRNynU=",
+    "https://media.gettyimages.com/photos/active-senior-woman-making-a-heart-with-her-hands-picture-id1167841965?k=6&m=1167841965&s=612x612&w=0&h=XEh6IzWChr_g9yPiF9Yh21OeqLNqr8ckOmA4aPUDXVc=",
+    "https://media.gettyimages.com/photos/proud-of-my-teams-achievements-picture-id1036148262?k=6&m=1036148262&s=612x612&w=0&h=AGcqZwVaob96a_GO5TV_dFswl_1qgJiq1Q2ZF0r-d_I=",
+    "https://media.gettyimages.com/photos/connection-with-nature-picture-id1174472274?k=6&m=1174472274&s=612x612&w=0&h=x4PpIA9bO4Ao_KA3LqA2AK7ewfF2aOggiUORenjlBMg=",
+    "https://media.gettyimages.com/photos/portrait-of-mature-woman-with-gray-hair-sitting-on-swing-picture-id986698776?k=6&m=986698776&s=612x612&w=0&h=Y1B-WL14Cs2X86Y448NJMowqhMDbSG3e158mcUqlyig=",
+    "https://media.gettyimages.com/photos/home-office-picture-id1193214720?k=6&m=1193214720&s=612x612&w=0&h=PG-IQkhXnBoKPFgErSLEwbDuAztvfXJjAg83tSr1RGA=",
+    "https://media.gettyimages.com/photos/succeeding-is-what-we-do-best-picture-id1140524579?k=6&m=1140524579&s=612x612&w=0&h=WLsjAp4MY6GclCeBZAPs53qekcgwbyHU8Qk0nhwqUxU=",
+    "https://media.gettyimages.com/photos/young-woman-spending-a-relaxing-day-in-her-beautiful-home-picture-id1062517720?k=6&m=1062517720&s=612x612&w=0&h=cOZzkiXa8eBpKCwTlOJsZrfVX_Sj87o_5d1q5sxPMRs=",
+    "https://media.gettyimages.com/photos/young-woman-with-arms-outstretched-in-carefree-moment-picture-id1132469935?k=6&m=1132469935&s=612x612&w=0&h=pEm_AX4hsTV5Az02VhpDTmQOd_v1eaBfejY7Sz3QFjw=",
+    "https://media.gettyimages.com/photos/man-taking-group-photo-of-family-at-bbq-picture-id1014773704?k=6&m=1014773704&s=612x612&w=0&h=966NRhhqoCR1yR9UPEN-4iqV7St2ujsxKC0KV4lkcqU=",
+    "https://media.gettyimages.com/photos/business-people-working-in-the-office-picture-id1089813056?k=6&m=1089813056&s=612x612&w=0&h=uPWZkcBw4KAwKdrdZqtjFoFd8LuhgRqs8YsMBHURhI0=",
+    "https://media.gettyimages.com/photos/group-of-friends-having-fun-picture-id1162987349?k=6&m=1162987349&s=612x612&w=0&h=1nSatpwHD-y77sl39RNWd8_prz6hx_U8_Qi8oJIteyI=",
+    "https://media.gettyimages.com/photos/inspire-your-teams-to-keep-on-achieving-picture-id1139054510?k=6&m=1139054510&s=612x612&w=0&h=iYhVNxhwU7RmPKJQhyexW2q0oZawpyiGCZp9N6yKYfw=",
+    "https://media.gettyimages.com/photos/portrait-of-woman-sitting-at-desk-in-design-office-picture-id1148558350?k=6&m=1148558350&s=612x612&w=0&h=7pyNhIWzrJCAlEHxj1ULzsupXpvmQN6EHWbyZQSGKn0=",
+    "https://media.gettyimages.com/photos/hispanic-female-studying-on-laptop-picture-id1182745546?k=6&m=1182745546&s=612x612&w=0&h=owhz2cLDARmdh7afK11rcaQZL36J2Vk4HESX3GytwUk=",
+    "https://media.gettyimages.com/photos/man-laughing-with-smart-phone-picture-id1189349093?k=6&m=1189349093&s=612x612&w=0&h=d9Cn7e02uKBTH0HwxC_le0kGLEidbbQCPenCQYpb42g=",
+    "https://media.gettyimages.com/photos/smiling-woman-using-laptop-at-the-bar-picture-id1135176767?k=6&m=1135176767&s=612x612&w=0&h=7V50eYgNFUeiUVmc-gSEVCQB2H7QNTt7mCWGWiSnRX4=",
+    "https://media.gettyimages.com/photos/handsome-indian-man-using-mobile-phone-picture-id1094067774?k=6&m=1094067774&s=612x612&w=0&h=P7ysJzmfaWZSzFarMw3jGLQMdVN1KL6CYvtBUIbQ1ck=",
+    "https://media.gettyimages.com/photos/smile-as-though-you-have-already-achieved-success-picture-id937239290?k=6&m=937239290&s=612x612&w=0&h=Xb38-VcMFsZDcRRptGRD5BrqcgJTJx_ziiD49IKUXnE=",
+    "https://media.gettyimages.com/photos/happy-mature-woman-looking-at-friend-in-forest-picture-id1007232098?k=6&m=1007232098&s=612x612&w=0&h=MocoJdHIshO-nGK0428Rjbd6NqJoiyN6alREvxcG-lE=",
+    "https://media.gettyimages.com/photos/successful-business-team-taking-selfie-picture-id1132119295?k=6&m=1132119295&s=612x612&w=0&h=5xFh6k0MZUepwdl8p7URCbkcR5vDPLvjvBBJQNxTBNA=",
+    "https://media.gettyimages.com/photos/business-video-conference-picture-id1184331589?k=6&m=1184331589&s=612x612&w=0&h=sUVd5ZBTrAwqZIbFSz0XE2sbc-FJzJOT4Kg3MHLAPXc=",
+    "https://media.gettyimages.com/photos/business-team-smiling-during-a-meeting-picture-id1146500509?k=6&m=1146500509&s=612x612&w=0&h=iL_umje_01DijV_YtgaGt6ZyezaR0AsJqMGYzq3apZc=",
+    "https://media.gettyimages.com/photos/laughing-woman-picture-id1138183258?k=6&m=1138183258&s=612x612&w=0&h=ECkpRqypJm5FfSzvoyrppsTYiFC_0uT7fIUjAQ_WEtg=",
+    "https://media.gettyimages.com/photos/beautiful-curly-hair-woman-picture-id992048656?k=6&m=992048656&s=612x612&w=0&h=jYOWFNEXeh-U1CLS_RrKW4FI4IQHL5WcMCy_nnKAslk=",
+    "https://media.gettyimages.com/photos/connected-in-comfort-picture-id1042058900?k=6&m=1042058900&s=612x612&w=0&h=IpToOKGB8sDaz50Imygy6hnNA6CO2JtdBa_b9AHuhMM=",
+    "https://media.gettyimages.com/photos/colourful-studio-portrait-of-a-young-woman-picture-id1145099485?k=6&m=1145099485&s=612x612&w=0&h=R94jYs1azrC40gVs0v9PB4favDyBuk7T_uRMCdhxZIU=",
+    "https://media.gettyimages.com/photos/portrait-business-women-in-the-office-picture-id919520858?k=6&m=919520858&s=612x612&w=0&h=SCLNf9lDUFY5hsH17-TWe1m_610HfhTa9A8I_emLHsE=",
+    "https://media.gettyimages.com/photos/portrait-of-mature-woman-laughing-picture-id901670390?k=6&m=901670390&s=612x612&w=0&h=BpVV4oPKNKj_2HJIOdaZ5vT9ImQRiSZvje9STtL_avc=",
+    "https://media.gettyimages.com/photos/you-make-me-happy-picture-id1178823473?k=6&m=1178823473&s=612x612&w=0&h=3__66kGPcZl84btUuEpKt_vrbqvKTCFsJIOLQhp_Jps=",
+    "https://media.gettyimages.com/photos/thinking-about-how-to-take-the-business-to-technological-heights-picture-id906798262?k=6&m=906798262&s=612x612&w=0&h=troEgYErvqNrlo2yfLSeQEGpatvsJow11j7roBJJHLw=",
+    "https://media.gettyimages.com/photos/family-sitting-on-pier-by-the-sea-picture-id1002673938?k=6&m=1002673938&s=612x612&w=0&h=7r58l3YSKvymCtNigkNUpBYLuVkix__mU4vETDCK63M=",
+    "https://media.gettyimages.com/photos/professionals-laughing-in-a-meeting-picture-id1150572098?k=6&m=1150572098&s=612x612&w=0&h=-EI95-Pziv3yOfh6EsxU-GT4l2SalmpHoId7511Dz2M=",
+    "https://media.gettyimages.com/photos/run-your-company-with-confidence-picture-id981750034?k=6&m=981750034&s=612x612&w=0&h=_ETPwPGwu2j49GVACQ-BCwkFsZSRugQFY_6FRnnaxQI=",
+    "https://media.gettyimages.com/photos/young-couple-with-smart-phone-relaxing-on-sofa-picture-id974230862?k=6&m=974230862&s=612x612&w=0&h=ZeOZHr3d4fzUuCy02jTq82AdfKiBdXpIzbvbOkieCTY=",
+    "https://media.gettyimages.com/photos/the-love-of-best-friends-picture-id972902038?k=6&m=972902038&s=612x612&w=0&h=Q5EbQ5RhL2ZXM3lnQobRgjEK0g8YM9T4hmbof0sBbf8=",
+    "https://media.gettyimages.com/photos/portrait-of-mature-man-dancing-and-having-fun-picture-id903060758?k=6&m=903060758&s=612x612&w=0&h=ReS7XMO8sumOGI3eaKvFP29Fy7SkUkOZOmxj_2fhH2A=",
+    "https://media.gettyimages.com/photos/happy-senior-woman-leading-her-family-to-perfect-picnic-place-on-the-picture-id1135289130?k=6&m=1135289130&s=612x612&w=0&h=-4MjxZhRquC2IUHCsf7mgXTkzSaLfSNBcQrBHwq7fa4=",
+    "https://media.gettyimages.com/photos/happy-woman-talking-on-phone-picture-id1127794824?k=6&m=1127794824&s=612x612&w=0&h=QAQm83IM0GyBTHQ_5HoTVSEkI6zWmwxYDFGoVgPXqNI=",
+    "https://media.gettyimages.com/photos/happy-kids-happy-family-picture-id1009877634?k=6&m=1009877634&s=612x612&w=0&h=7z6yRXGO_JcC3QnMvuaf4RpSVRjjDgybsITYmrByjxE=",
+    "https://media.gettyimages.com/photos/the-success-of-a-business-leans-on-its-team-picture-id1023552044?k=6&m=1023552044&s=612x612&w=0&h=Lt2vXZMKySPDLyW2KqVpQoKQKoDIhk7J7xu4UfMsN7Q=",
+    "https://media.gettyimages.com/photos/road-trips-put-me-in-a-happy-mood-picture-id1133563360?k=6&m=1133563360&s=612x612&w=0&h=kAXQSco-g3SqfUljxibcTvSyXJ4G007sZ7GS0B5F4Yo=",
+    "https://media.gettyimages.com/photos/always-be-ready-when-business-comes-calling-picture-id1144585582?k=6&m=1144585582&s=612x612&w=0&h=JvlDU9yUEvsPNk0nP8MprU8QXXbgS3jN9TjmMjnhmfo=",
+    "https://media.gettyimages.com/photos/this-team-is-built-for-success-picture-id629667248?k=6&m=629667248&s=612x612&w=0&h=61gZlmPtT5Ta9sTaKNDaExHi2yZypt5NeDOPfEF7qTQ=",
+    "https://media.gettyimages.com/photos/boy-and-dog-in-toy-racing-car-picture-id1162019476?k=6&m=1162019476&s=612x612&w=0&h=3FWAHt58LjBXH_oKNqG7kB45ULUsH1MLmLl5Cpea7ek=",
+    "https://media.gettyimages.com/photos/mature-couple-relaxing-with-tablet-and-smartphone-picture-id915361676?k=6&m=915361676&s=612x612&w=0&h=WGUTZqek_i9eINKvfxOcmXX5MGkzRT3I-tWFNbNGaOU=",
+    "https://media.gettyimages.com/photos/portrait-of-a-beautiful-young-indian-woman-picture-id917499044?k=6&m=917499044&s=612x612&w=0&h=lx17asXD4jaIlXrFgBdd8Tinl0oSe43K0All11IjKWY=",
+  ];
 
   const session = driver.session();
   const userRepository = new UserRepository(driver);
@@ -90,32 +152,31 @@ async function createUser(Imgs: any, index: number): Promise<User> {
   return user;
 }
 
-async function scrapeImages(url : string): Promise<any> {
+async function scrapeImages(url: string): Promise<any> {
   let browser = await puppeteer.launch({
-      headless: true,
-      args: ["--disable-setuid-sandbox"],
-      'ignoreHTTPSErrors': true
+    headless: true,
+    args: ["--disable-setuid-sandbox"],
+    ignoreHTTPSErrors: true,
   });
 
   const page = await browser.newPage();
   page.goto(url);
   await page.setViewport({
-      width: 1000,
-      height: 800
+    width: 1000,
+    height: 800,
   });
 
-  await page.waitForSelector('.search-content__gallery');
+  await page.waitForSelector(".search-content__gallery");
 
   try {
-      const imgUrl = await page.$$eval(
-          '.search-content__gallery .gallery-mosaic-asset__figure img', 
-          (imgs : any) => imgs.map((img: any) => img.src)
-      );
-      console.log('Scrapped User Images');
-      
-      return imgUrl;
-  } catch (err) {
-      console.log("Went wrong", err);
-  }
+    const imgUrl = await page.$$eval(
+      ".search-content__gallery .gallery-mosaic-asset__figure img",
+      (imgs: any) => imgs.map((img: any) => img.src)
+    );
+    console.log("Scrapped User Images");
 
+    return imgUrl;
+  } catch (err) {
+    console.log("Went wrong", err);
+  }
 }
